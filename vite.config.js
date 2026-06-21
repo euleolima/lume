61273c350558261f6f877fe450e0866c47301b45
euleolima <hello.leonardolmonteiro@gmail.com>
2026-06-21 15:53:49 -0300

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   // Se hospedar em github.com/SEU_USUARIO/lume, troque '/' por '/lume/'
-  base: '/',
+  base: '/lume/',
   plugins: [
     react(),
     VitePWA({
